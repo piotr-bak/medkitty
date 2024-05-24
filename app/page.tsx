d7d592cc35e71c30966595baf87fa70798ac2291
piotr-bak <68067947+piotr-bak@users.noreply.github.com>
2024-05-24 02:07:21 +0200

    feat: added Sign in button

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { SignInButton } from "./_components/SignInButton";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
+        <SignInButton />
         <div>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
