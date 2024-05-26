095d47aa66bfdebe93c16846e4a1856e4238d009
piotr-bak <68067947+piotr-bak@users.noreply.github.com>
2024-05-26 12:20:54 +0200

    feat: pet list screen - wip

--- app/_components/PetList/PetList.tsx
+++ app/_components/PetList/PetList.tsx
@@ -1,10 +1,11 @@
 'use client'
 import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
+import type { Pet } from "@/app/_types";
 
 export function PetList() {
     const { data: session, status } = useSession();
-    const [pets, setPets] = useState( [] );
+    const [pets, setPets] = useState<Pet[]>( [] );
 
     useEffect( () => {
         const fetchPets = async () => {
@@ -18,8 +19,20 @@ export function PetList() {
 
     return (
         <div>
-            here will be PetList
-            {JSON.stringify( pets )}
+            {status === 'loading' && <p>loading data...</p>}
+            {( status !== 'loading' && pets ) && (
+                <ul>
+                    {pets.map( pet => {
+                        return (
+                            <li key={pet.id}>
+                                <p>{pet.name}</p>
+                                <button aria-label="see more">{'->'}</button>
+                            </li>
+                        )
+                    } )}
+                </ul>
+            )}
+            <button aria-label="add pet">{'+'}</button>
         </div>
     )
 }
---
+++ app/_types/Pet.ts
@@ -0,0 +1,8 @@
+export interface Pet {
+    id: string;
+    name: string;
+    breed: string;
+    age: number;
+    owners: string[];
+    medicationSchedules: string[];
+}
