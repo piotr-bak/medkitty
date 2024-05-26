86af9b001a274bee957d7c8ea343f29eb90b2060
piotr-bak <68067947+piotr-bak@users.noreply.github.com>
2024-05-26 13:05:31 +0200

    feat: added pet details to the dashboard - wip

--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -1,10 +1,12 @@
 import { PetList } from '@/app/_components/PetList/PetList';
 import styles from './page.module.scss';
+import { PetDetails } from '@/app/_components/PetDetails/PetDetails';
 
 export default function Dashboard() {
     return (
         <div className={styles.dashboard} >
             <PetList />
+            <PetDetails />
         </div>
     )
 }
