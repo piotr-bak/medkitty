a3f344c0ddb4473e96e1b69a17f485ef179bbe3b
piotr-bak <68067947+piotr-bak@users.noreply.github.com>
2024-06-02 19:09:50 +0200

    feat: interface layout - wip

--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -1,15 +1,62 @@
+'use client'
 import { PetList } from '@/app/_components/PetList/PetList';
 import styles from './page.module.scss';
 import Link from 'next/link';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css'
+import 'swiper/css/pagination'
+import { Pagination } from 'swiper/modules';
 
 export default function Page() {
 
     return (
-        <div className={styles.dashboard} >
-            <PetList />
-            <Link href={'/dashboard/pet'}>
-                <button className={styles.buttonAdd} aria-label="add pet">{'+'}</button>
-            </Link>
-        </div>
+        <Swiper
+            className={styles.swiper}
+            modules={[Pagination]}
+            spaceBetween={25}
+            pagination={{
+                clickable: true,
+            }}
+        >
+            <SwiperSlide className={styles.panel}>
+                <Swiper
+                    className={styles.swiper}
+                    direction={'vertical'}
+                    modules={[Pagination]}
+                    spaceBetween={25}
+                    pagination={{
+                        clickable: true,
+                    }}
+                >
+                    <SwiperSlide className={styles.panel}>
+                        Summary
+                    </SwiperSlide>
+                    <SwiperSlide className={styles.panel}>
+                        <section>
+                            <PetList />
+                            <Link href={'/dashboard/pet'}>
+                                <button className={styles.buttonAdd} aria-label="add pet">{'+'}</button>
+                            </Link>
+                        </section>
+                    </SwiperSlide>
+                </Swiper>
+            </SwiperSlide>
+            <SwiperSlide className={styles.panel}>
+                Admin
+            </SwiperSlide>
+        </Swiper>
     )
 }
+
+
+// export default function Page() {
+
+//     return (
+//         <div className={styles.dashboard} >
+//             <PetList />
+//             <Link href={'/dashboard/pet'}>
+//                 <button className={styles.buttonAdd} aria-label="add pet">{'+'}</button>
+//             </Link>
+//         </div>
+//     )
+// }
