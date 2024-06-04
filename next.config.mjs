84a84603110da8ac82f72e6173d0b6fcfd6c096e
piotr-bak <68067947+piotr-bak@users.noreply.github.com>
2024-06-04 10:20:18 +0200

    chore: updated configs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    transpilePackages: ['@mui/lab'],
+    modularizeImports: {
+        '@mui/lab': {
+            transform: '@mui/lab/{{member}}'
+        }
+    }
+};
 
 export default nextConfig;
