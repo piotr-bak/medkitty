a5272f05bd22129ee574b440386a50ee99db6ce1
piotr-bak <68067947+piotr-bak@users.noreply.github.com>
2024-06-05 22:19:44 +0200

    feat: added endpoint for GETting the schedule data

--- app/api/pets/plan/route.ts
+++ app/api/pets/plan/route.ts
@@ -3,6 +3,62 @@ import { getSession } from "@auth0/nextjs-auth0";
 import { ONE_DAY_IN_MS } from "@/app/_lib/constants/oneDayInMiliseconds";
 import prisma from "@/app/_lib/prisma";
 
+export async function GET(request: Request) {
+    const session = await getSession();
+
+    if (!session?.user)
+        return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+
+    const user = await prisma.user.findUnique({
+        where: {
+            foreignId: session.user.sub,
+        },
+    });
+
+    if (!user) {
+        return NextResponse.json({ error: "User not found" }, { status: 404 });
+    }
+
+    const { searchParams } = new URL(request.url);
+    const petId = searchParams.get("id");
+
+    try {
+        if (petId) {
+            const medicationPlan = await prisma.medicationPlan.findFirst({
+                where: {
+                    petId: petId,
+                    userId: user.id,
+                },
+                include: {
+                    days: {
+                        include: {
+                            doses: {
+                                include: {
+                                    medication: true,
+                                },
+                            },
+                        },
+                    },
+                },
+            });
+
+            if (!medicationPlan)
+                return NextResponse.json(
+                    { error: "Pet not found or no plan created yet" },
+                    { status: 404 }
+                );
+
+            return NextResponse.json(medicationPlan.days);
+        }
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json(
+            { error: "Internal Server Error" },
+            { status: 500 }
+        );
+    }
+}
+
 export async function POST(request: Request) {
     const session = await getSession();
 
