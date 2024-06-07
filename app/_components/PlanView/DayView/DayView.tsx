08f35c711b4c4642e09808dc8c99622c5bfcf32b
piotr-bak <68067947+piotr-bak@users.noreply.github.com>
2024-06-07 03:32:42 +0200

    feat: individual dose display - wip

--- app/_components/PlanView/DayView/DayView.tsx
+++ app/_components/PlanView/DayView/DayView.tsx
@@ -1,13 +1,14 @@
 import type { Day } from '@/app/_types';
 import styles from './DayView.module.scss';
+import { useFetch } from '@/app/_lib/hooks/useFetch';
 
-export function DayView({ dayData }: { dayData: Day }) {
+export function DayView( { dayData }: { dayData: Day } ) {
     const { id, medicationPlanId: planId, date, doses } = dayData;
     return (
         <>
             <div className={styles.panel}>
-                {/* <p>{`${day.date}`}</p>
-                {JSON.stringify( day )} */}
+                <p>{`${date}`}</p>
+                {JSON.stringify( doses )}
             </div>
         </>
     );
