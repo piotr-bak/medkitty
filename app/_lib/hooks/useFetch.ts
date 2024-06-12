ef20587d5f4facd9b95a4669253db32080e9003e
piotr-bak <68067947+piotr-bak@users.noreply.github.com>
2024-06-13 01:54:59 +0200

    feat: fetch now uses swr under the hood

--- app/_lib/hooks/useFetch.ts
+++ app/_lib/hooks/useFetch.ts
@@ -1,50 +1,49 @@
-import { useEffect, useState } from 'react';
+import useSWR, { mutate } from 'swr';
+import type { SWRConfiguration } from 'swr';
 import serverErrors from './httpErrorCodes';
 
 interface FetchOptions extends RequestInit {
     logErrors?: boolean;
 }
 
-export function useFetch<T>(url: string, options?: FetchOptions) {
-    const [data, setData] = useState<T | null | undefined>(undefined);
-    const [isError, setIsError] = useState(false);
-    const [isLoading, setIsLoading] = useState(true);
+const fetcher = async ( url: string, options?: FetchOptions ) => {
+    try {
+        const response = await fetch( url, options );
 
-    useEffect(() => {
-        setIsLoading(true);
-        setData(undefined);
-        setIsError(false);
+        if ( !response.ok ) {
+            const message = `Invalid server response. Error ${response.status}: ${serverErrors.get( response.status )}`;
+            throw new Error( message );
+        }
 
-        const controller = new AbortController();
+        const data = await response.json();
+        return data !== undefined ? data : null; // Ensure null if no data is received
+    } catch ( error ) {
+        if ( options?.logErrors !== false && error instanceof Error ) {
+            console.error( error.message );
+        }
+        throw error;
+    }
+};
 
-        (async () => {
-            try {
-                const response = await fetch(url, {
-                    signal: controller.signal,
-                    ...options,
-                });
-                if (!response.ok) {
-                    const message = `Invalid server response. Error ${response.status}: ${serverErrors.get(response.status)}`;
-                    setData(null);
-                    throw new Error(message);
-                } else {
-                    const json = await response.json();
-                    setData(json);
-                }
-            } catch (error) {
-                if (error instanceof Error) {
-                    if ('AbortError' === error.name) return;
-                    setIsError(true);
-                    if (options?.logErrors !== false)
-                        console.error(error.message);
-                }
-            } finally {
-                setIsLoading(false);
-            }
-        })();
+export function useFetch<T>( url: string, options?: FetchOptions & SWRConfiguration ) {
+    const { data, error, isValidating } = useSWR<T>( url, () => fetcher( url, options ), { ...options, shouldRetryOnError: false } );
 
-        return () => controller.abort();
-    }, [url, options]);
+    // Undefined while fetching, null if fetch complete but no data received
+    let formattedData: T | null | undefined = data;
 
-    return { data, isError, isLoading };
+    if ( isValidating && formattedData === undefined ) {
+        formattedData = undefined;
+    } else if ( !isValidating && data === undefined ) {
+        formattedData = null;
+    }
+
+    return {
+        data: formattedData,
+        isError: !!error,
+        isLoading: isValidating && !formattedData,
+    };
+}
+
+export function revalidateFetch( url: string ) {
+    mutate( url );
 }
