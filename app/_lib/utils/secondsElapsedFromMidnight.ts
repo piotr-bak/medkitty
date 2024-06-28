98de8f3bfb7329e34f72cc81140f0e5fdc80ce62
piotr-bak <68067947+piotr-bak@users.noreply.github.com>
2024-06-28 11:08:26 +0200

    feat: added better error handling

--- app/_lib/utils/secondsElapsedFromMidnight.ts
+++ app/_lib/utils/secondsElapsedFromMidnight.ts
@@ -4,11 +4,20 @@ import utc from 'dayjs/plugin/utc';
 dayjs.extend( utc );
 
 export async function secondsElapsedFromMidnight( time: string ): Promise<number> {
-    if ( !time ) return;
+    if ( !time ) {
+        throw new Error( 'Valid time has not been provided!' );
+    }
+
     const [hoursStr, minutesStr] = time.split( ':' );
+    if ( hoursStr === undefined || minutesStr === undefined ) {
+        throw new Error( 'Time must be in the format HH:mm' );
+    }
 
     const hours = Number( hoursStr );
     const minutes = Number( minutesStr );
+    if ( isNaN( hours ) || isNaN( minutes ) ) {
+        throw new Error( 'Hours and minutes must be valid numbers' );
+    }
 
     const timeObject = dayjs.utc().hour( hours ).minute( minutes ).second( 0 );
     const startOfDay = dayjs.utc().startOf( 'day' );
